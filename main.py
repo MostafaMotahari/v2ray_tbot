7fb080440943f23de42e13616e9a8e068aa65d43
Mostafa Motahari <mostafamotahari2004@gmail.com>
2022-11-30 20:10:43 +0330

    ADD: Database query

--- main.py
+++ main.py
@@ -46,48 +46,48 @@ def get_vpn(client: Client, callback_query: CallbackQuery):
     rand_uuid = str(uuid.uuid4())
 
     settings = '''{
-      "clients": [
-          {
-          "id": "''' + rand_uuid + '''",
-          "alterId": 0,
-          "email": "",
-          "limitIp": 0,
-          "totalGB": 0,
-          "expiryTime": ""
-          }
-      ],
-      "disableInsecureEncryption": false
-    }'''
+  "clients": [
+      {
+      "id": "''' + rand_uuid + '''",
+      "alterId": 0,
+      "email": "",
+      "limitIp": 0,
+      "totalGB": 0,
+      "expiryTime": ""
+      }
+  ],
+  "disableInsecureEncryption": false
+}'''
 
     stream_settings = '''{
-      "network": "ws",
-      "security": "tls",
-      "tlsSettings": {
-          "serverName": "''' + _tls_domain + '''",
-          "certificates": [
-          {
-              "certificateFile": "/ets/lsdfhsdlkjfksldf/dsfdf",
-              "keyFile": "/sdfds/lsdkfj/dfsd"
-          }
-          ],
-          "alpn": []
-      },
-      "wsSettings": {
-          "acceptProxyProtocol": false,
-          "path": "/",
-          "headers": {}
+  "network": "ws",
+  "security": "tls",
+  "tlsSettings": {
+      "serverName": "''' + _tls_domain + '''",
+      "certificates": [
+      {
+          "certificateFile": "/ets/lsdfhsdlkjfksldf/dsfdf",
+          "keyFile": "/sdfds/lsdkfj/dfsd"
       }
-    }'''
+      ],
+      "alpn": []
+  },
+  "wsSettings": {
+      "acceptProxyProtocol": false,
+      "path": "/",
+      "headers": {}
+  }
+}'''
 
     siniffing = """{
-      "enabled": true,
-      "destOverride": [
-          "http",
-          "tls"
-      ]
-    }"""
+  "enabled": true,
+  "destOverride": [
+      "http",
+      "tls"
+  ]
+}"""
     
-    cursor = conn.execute(f"INSERT INTO inbounds (user_id, up, down, total, remark, enable, expiry_time, listen, port, protocol, settings, stream_settings, tag, sniffing ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (1, 0, 0, 0, f'u{callback_query.from_user.id}', 1, 0, '', 25007, 'v2ray', settings, stream_settings, f'inbound-{callback_query.from_user.id}', siniffing))
+    cursor = conn.execute(f"INSERT INTO inbounds (user_id, up, down, total, remark, enable, expiry_time, listen, port, protocol, settings, stream_settings, tag, sniffing ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (1, 0, 0, 0, f'u{callback_query.from_user.id}', 1, 0, '', 25007, 'vmess', settings, stream_settings, f'inbound-{callback_query.from_user.id}', siniffing))
 
     conn.commit()
     conn.close()
@@ -96,8 +96,8 @@ def get_vpn(client: Client, callback_query: CallbackQuery):
 
     callback_query.edit_message_text(
         "Your account is ready, please scan the QR code below to get your account."
-        "If you can't scan the QR code, please copy the link below and paste it to your v2ray client."
-        "```code : {}```".format(v2ray_qrcode),
+        "If you can't scan the QR code, please copy the link below and paste it to your v2ray client.\n\n" + v2ray_qrcode
+        ,
     )
 
 app.run()
